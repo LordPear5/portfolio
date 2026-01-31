05bbf747bca07c270813a4ce1348626e5911890f
LordPear5 <annepasz993@gmail.com>
2026-01-31 15:14:56 +0100

    remove temp change

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -7,14 +7,6 @@ export default function About() {
         I'm a very creative person who is very interested in art. This website
         is dedicated to the painters that has inspired me throughout my life.
       </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quia ut fugit
-        facilis possimus similique eos voluptas iusto quasi reprehenderit
-        architecto, natus est a totam ipsum, accusantium temporibus, ipsa in
-        neque cumque cupiditate doloribus asperiores unde culpa aliquid.
-        Corrupti exercitationem delectus, natus culpa, saepe illum veritatis est
-        ullam eligendi voluptatem porro!
-      </p>
     </section>
   );
 }
